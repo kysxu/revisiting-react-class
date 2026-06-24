f51c286890b3610527ced2e5b540bdd94e78548d
KT <felikzz98@gmail.com>
2026-06-24 11:27:28 +0700

    complete mission-3

--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -1,4 +1,4 @@
-import ProductCard from "./ProductCard";
+import ProductCard from "./ProductCard.jsx";
 
 function ProductHighlight() {
   return (
@@ -6,16 +6,24 @@ function ProductHighlight() {
       <div className="container mx-auto">
         <h2 className="text-2xl font-semibold mb-4">Featured Products</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {/* 🛠️ Todo 2: ให้ส่งข้อมูลเข้าไปใน Component ProductCard ผ่าน Prop ที่ชื่อว่า `imgSrc`
-            
-            ==== ตัวอย่าง ====
-
-            <ProductCard imgSrc="https://picsum.photos/id/231/200/200" />
-            
-        */}
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {/* Product Card 1 */}
+          <ProductCard
+            imgSrc="https://picsum.photos/id/231/200/200"
+            productName="The Lorem Ipsum for photos."
+            price="99.99"
+          />
+          {/* Product Card 2 */}
+          <ProductCard
+            imgSrc="https://picsum.photos/id/232/200/200"
+            productName="Wireless Bluetooth Earbuds"
+            price="79.99"
+          />
+          {/* Product Card 3 */}
+          <ProductCard
+            imgSrc="https://picsum.photos/200/200"
+            productName="Stylish Cool Sunglasses in The World"
+            price="79.99"
+          />
         </div>
       </div>
     </section>
